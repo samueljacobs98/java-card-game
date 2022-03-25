eb3f673ca521e03f85301c574a1b4c3399ff00e2
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-03-25 11:07:17 +0000

    completed stage 2 git push

--- src/com/company/CardGame.java
+++ src/com/company/CardGame.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class CardGame {
     private ArrayList<Card> deckOfCards = new ArrayList<Card>(0);
@@ -15,32 +17,51 @@ public class CardGame {
         return deckOfCards;
     }
 
+    private String getSuitFromSuitNum(int suitNum) {
+        switch (suitNum) {
+            case 0:
+                return "♥";
+            case 1:
+                return "♦";
+            case 2:
+                return "♠";
+            case 3:
+                return "♣";
+            default:
+                return "-1";
+            // Change to throw exception rather than default
+        }
+    }
+
     private void initialiseDeck() {
-        for (int suitNum = 0; suitNum < 4; suitNum++ ) {
-            String suit;
-            switch (suitNum) {
-                case 0:
-                    suit = "♥";
-                    break;
-                case 1:
-                    suit = "♦";
-                    break;
-                case 2:
-                    suit = "♠";
-                    break;
-                case 3:
-                    suit = "♣";
-                    break;
-                default:
-                    suit = "-1";
-                    break;
-                // Change to throw exception rather than default
-            }
-            for (int value = 2; value < 15; value++) {
+        for (int value = 2; value < 15; value++) {
+            for (int suitNum = 0; suitNum < 4; suitNum++ ) {
+            String suit = getSuitFromSuitNum(suitNum);
                 deckOfCards.add(new Card(suit, value));
             }
         }
     }
 
+    public Card dealCard() {
+        return deckOfCards.get(0);
+    }
 
+    public void sortDeckInNumberOrder() {
+        deckOfCards.removeAll(deckOfCards);
+        initialiseDeck();
+    }
+
+    public void sortDeckIntoSuits() {
+        deckOfCards.removeAll(deckOfCards);
+            for (int suitNum = 0; suitNum < 4; suitNum++ ) {
+                String suit = getSuitFromSuitNum(suitNum);
+                for (int value = 2; value < 15; value++) {
+                    deckOfCards.add(new Card(suit, value));
+                }
+            };
+    }
+
+    public void shuffleDeck() {
+                Collections.shuffle(deckOfCards);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         CardGame cardGame = new CardGame("poker");
-        ArrayList<Card> deck = cardGame.getDeck();
+        System.out.println(cardGame.getDeck());
+        cardGame.shuffleDeck();
+        System.out.println(cardGame.getDeck());
+        System.out.println(cardGame.dealCard());
+        cardGame.sortDeckIntoSuits();
+        System.out.println(cardGame.getDeck());
+        cardGame.sortDeckInNumberOrder();
+        System.out.println(cardGame.getDeck());
+//        ArrayList<Card> deck = cardGame.getDeck();
 //        for (Card card : deck){
 //            System.out.println(card.toString());
 //        };
     }
 }
+
+//        Log all cards
+//        System.out.println(cardGame.getDeck());
+
