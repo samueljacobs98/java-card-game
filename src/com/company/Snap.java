5516bd446a4800160722f6a28607ca4003502ef2
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-03-25 12:36:07 +0000

    completed stage 4a

--- src/com/company/Snap.java
+++ src/com/company/Snap.java
@@ -14,20 +14,32 @@ public class Snap extends CardGame{
         shuffleDeck();
 
         boolean gameStarted = false;
+        Player player1 = new Player("");
+        Player player2 = new Player("");
 
         while (!gameStarted) {
+            System.out.println("Player 1: Enter your name");
+            player1.setName(scanner.nextLine());
+
+            System.out.println("Player 2: Enter your name");
+            player2.setName(scanner.nextLine());
+
+            System.out.println("Hello " + player1.getName() + " and " + player2.getName() + ".");
             System.out.println("Press enter to begin");
             String input = scanner.nextLine();
-            if (input.length() == 0) {
-                gameStarted = true;
-            }
+
+            if (input.length() == 0) gameStarted = true;
         }
 
         boolean winner = false;
         Card previousCard = new Card("",-1);
 
+        boolean player1Turn = true;
+        Player currentPlayer = player1;
+
         while (getDeck().size() > 0 && !winner){
-            System.out.println("Press enter to flip the next card");
+            currentPlayer = player1Turn ? player1 : player2;
+            System.out.println(currentPlayer.getName() + ": Press enter to flip the next card");
             String input = scanner.nextLine();
             if (input.length() == 0) {
                 Card newCard = dealCard();
@@ -37,9 +49,10 @@ public class Snap extends CardGame{
                 }
                 previousCard = newCard;
             }
+            player1Turn = !player1Turn;
         }
 
-        String endMessage = winner ? "You win!" : "You lose!";
+        String endMessage = winner ? "Snap! " + currentPlayer.getName() + " wins!" : "You both lose!";
         System.out.println(endMessage);
     }
 }
